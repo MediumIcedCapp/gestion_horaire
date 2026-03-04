78ff42c257750ca5f9bfa6a59eded1da4eddd451
Mahad03 <149258511+Mahad03@users.noreply.github.com>
2026-03-04 13:52:47 -0500

    Ajout de la page d'accueil

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate, Link } from "react-router-dom";
 import Login from "./pages/connexion/login.jsx";
 import SignUp from "./pages/connexion/SignUp.jsx";
+import Accueil from "./Accueil.jsx";
 
 
 function App() {
   return (
       <Routes>
+        <Route path="/" element={<Accueil />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
