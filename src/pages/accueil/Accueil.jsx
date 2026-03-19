a76876ee74192ee43022e980cba633186b1bc5d1
Danick Auger <danick269@gmail.com>
2026-03-19 10:11:06 -0400

    Ajout de style a la page accueil

--- src/pages/accueil/Accueil.jsx
+++ src/pages/accueil/Accueil.jsx
@@ -5,12 +5,11 @@
 import React from 'react'
 import styles from './Accueil.module.css'
 import Footer from '../../composantes/Footer'
+import Logo from "../../assets/logoGestionHoraire.png";
+import ShowcaseImage from "../../assets/AccueilCalendarShowcase.jpg";
 
 export default function Accueil() {
     console.log("home load")
-
-    var temp = null
-    var x = 0
 
     // fonction creer compte
     function creerCompte() {
@@ -27,34 +26,32 @@ export default function Accueil() {
 
 return (
     <div className={styles.accueil_page}>
-        <div className={styles.logo_container}>
-            <img src="/logo.png" alt="LOGO" className={styles.logo} />
-        </div>
+        <div className={styles.accueil_container}>
 
-        <div className={styles.main_section}>
-            <h1 className={styles.titre_principal} style={{color: 'darkblue'}}>Bienvenu Sur Notre Site!</h1>
-            <p className={styles.sous_titre}>Pour votre planification quotidienne!</p>
+            <div className={styles.main_section}>
 
-            <div className={styles.boutons_container}>
-                <button className={styles.bouton_principal} onClick={creerCompte}>Creer Un Compte</button>
-                <button className={styles.bouton_principal} onClick={seConnecter}>Se connecter</button>
-            </div>
-        </div>
-
-        <div className={styles.section_bas}>
-            <div className={styles.contenu_bas}>
-                <div className={styles.image_gauche}>
-                    <img src="/logo.png" alt="logo" className={styles.logo_bas} />
+                <div className={styles.header_section}>
+                    <img src={Logo} alt="LOGO" className={styles.logo} />
+                    <h1 className={styles.titre_principal}>PLANIGO</h1>
+                    <p className={styles.sous_titre}>Pour votre planification quotidienne!</p>
                 </div>
-                <div className={styles.texte_droite}>
-                    <h2>Gestion de calendrier gratuit</h2>
-                    <p>Description</p>
+
+                <div className={styles.boutons_container}>
+                    <button className={styles.bouton_principal} onClick={creerCompte}>Creer Un Compte</button>
+                    <button className={styles.bouton_principal} onClick={seConnecter}>Se connecter</button>
                 </div>
             </div>
-        </div>
 
-        <div className={Footer}>
-            <Footer />
+            <div className={styles.section_description}>
+                    <img src={ShowcaseImage} alt="Showcase" className={styles.showcase_image} />
+                    <div className={styles.texte_description}>
+                        <h2>Gestion de calendrier gratuit</h2>
+                        <p>Description</p>
+                    </div>
+            </div>
+            <div className={styles.footer}>
+                <Footer />
+            </div>
         </div>
     </div>
 )
