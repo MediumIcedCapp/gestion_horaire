cb0737dbbd0c6fa296862faf7c0f294731dcda74
Danick Auger <danick269@gmail.com>
2026-04-22 21:55:46 -0400

    Update baseDeDonnees.js

--- config/baseDeDonnees.js
+++ config/baseDeDonnees.js
@@ -1,19 +1,38 @@
 //Importations
 import mysql from "mysql2";
 
-const baseDeDonnees = mysql.createConnection({
+const baseDeDonnees = mysql.createPool({
   host: "localhost",
   user: "root",
   password: "", 
-  database: "gestion_horaire"
+  database: "gestion_horaire",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
 });
 
-baseDeDonnees.connect((err) => {
+const gererErreurConnexion = (err) => {
+  console.error("Erreur connexion MySQL:", err);
+};
+
+baseDeDonnees.on("connection", (connection) => {
+  connection.on("error", gererErreurConnexion);
+});
+
+baseDeDonnees.getConnection((err, connection) => {
   if (err) {
-    console.log("Une erreur s'est produite: ", err);
+    console.error("Une erreur s'est produite: ", err);
   } else {
     console.log("Connexion à la base de données réussite");
+    connection.on("error", gererErreurConnexion);
+    connection.release();
   }
+});
+
+baseDeDonnees.on("error", (err) => {
+  console.error("Erreur pool MySQL:", err);
 });
 
 export default baseDeDonnees;
